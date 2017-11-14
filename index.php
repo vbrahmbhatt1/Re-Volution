48abbbdc9aa8f85bfecb44cbf85bf17725e73de4
vbrahmbhatt1 <vbrahmbhatt1@luc.edu>
2017-11-13 21:27:07 -0600

    p

--- index.php
+++ index.php
@@ -1,38 +1,28 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-<div id="particleCanvas-Blue"></div>
-<div id="particleCanvas-White"></div>
-<meta charset="UTF-8" />
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-<title>Music Giant</title>
-<link rel="stylesheet" href="css/style2.css">
-<!-- Google fonts -->
-<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,700' rel='stylesheet' type='text/css'>
-<!-- font awesome -->
-<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-<!-- bootstrap -->
-<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
-<!-- animate.css -->
-<link rel="stylesheet" href="assets/animate/animate.css" />
-<link rel="shortcut icon" href="images/logo.jpg" type="image/x-icon">
-<link rel="icon" href="images/logo.jpg" type="image/x-icon">
-<link rel="stylesheet" type="text/css" href="assets/slidingheader/layout-simple.css" />
-<link rel="stylesheet" href="assets/style.css">
-</head>
-<body id="home">
-		<div id="container">
+<html lang="en" class="no-js">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Music Giant</title>
+		<link rel="shortcut icon" href="../favicon.ico">
+		<link rel="stylesheet" type="text/css" href="css/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="css/layout-simple.css" />
+	</head>
+	<body>
+		<div id="container" class="container">
 			<header class="intro">
+				<img class="intro__image" src="images/1.jpg" >
 				<div class="intro__content">
-					<h1>Re-Volution</h1>
-						<button class="trigger">
+					<h1 class="intro__title">Re-Volution</h1>
+					<div class="intro__subtitle">
+							<button class="trigger">
 							<svg width="100%" height="100%" viewBox="0 0 60 60" preserveAspectRatio="none">
 								<g class="icon icon--grid">
-									<rect x="32.5" y="5.5" width="22" height="22"/>
-									<rect x="4.5" y="5.5" width="22" height="22"/>
-									<rect x="32.5" y="33.5" width="22" height="22"/>
-									<rect x="4.5" y="33.5" width="22" height="22"/>
+									<rect x="30" y="5.5" width="20" height="20"/>
+									<rect x="4" y="5.5" width="20" height="20"/>
+									<rect x="30" y="33.5" width="20" height="20"/>
+									<rect x="4" y="33.5" width="20" height="20"/>
 								</g>
 								<g class="icon icon--cross">
 									<line x1="4.5" y1="55.5" x2="54.953" y2="5.046"/>
@@ -41,70 +31,57 @@
 							</svg>
 							<span>View content</span>
 						</button>
-				</div>
-			</header>
+					</div>
+				</div><!-- /intro__content -->
+			</header><!-- /intro -->
 			<section class="items-wrap">
-			<div class="container">
-				<div class="spacer">
-				<div class="row">
-					<div class="col-sm-4 fadeInUp wowload">
-						<div class="gallery-items">
-						<img class="img-responsive" src="images/ice/1.jpg" alt="item01"/>
-						<div class="caption"><h3>Sunny</h3> <a class="gallery-image btn btn-default" data-gallery>+</a></div>
-						</div>
-					</div>
-					<div class="col-sm-4 fadeInUp wowload">
-						<div class="gallery-items">
-						<img class="img-responsive" src="images/ice/2.jpg" alt="item01"/>
-						<div class="caption"><h3>Rainy</h3> <a class="gallery-image btn btn-default" data-gallery>+</a></div>
-						</div>
-					</div>
-					<div class="col-sm-4 fadeInUp wowload">
-						<div class="gallery-items">
-						<img class="img-responsive" src="images/ice/3.jpg" alt="item01"/>
-						<div class="caption"><h3>Snowy</h3> <a class="gallery-image btn btn-default" data-gallery>+</a></div>
-						</div>
-					</div>
-					<div class="col-sm-4 fadeInUp wowload">
-						<div class="gallery-items">
-						<img class="img-responsive" src="images/ice/4.jpg" alt="item01"/>
-						<div class="caption"><h3>Windy</h3> <a class="gallery-image btn btn-default" data-gallery>+</a></div>
-						</div>
-					</div>
-					<div class="col-sm-4 fadeInUp wowload">
-						<div class="gallery-items">
-						<img class="img-responsive" src="images/ice/5.jpg" alt="item01"/>
-						<div class="caption"><h3>Cloudy</h3> <a class="gallery-image btn btn-default" data-gallery>+</a></div>
-						</div>
-					</div>
-					<div class="col-sm-4 fadeInUp wowload">
-						<div class="gallery-items">
-						<img class="img-responsive" src="images/ice/6.jpg" alt="item01"/>
-						<div class="caption"><h3>Icy</h3> <a  class="gallery-image btn btn-default" data-gallery>+</a></div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
-		<form>  <input type="text" name="search" placeholder="Search.."></form>
-</div>
-
-<!-- jquery -->
-<script src="assets/jquery.js"></script>
-<!-- wow script -->
-<script src="assets/wow/wow.min.js"></script>
-<!-- slider -->
-<script src="assets/slidingheader/classie.js"></script>
-<!-- boostrap -->
-<script src="assets/bootstrap/js/bootstrap.js" type="text/javascript" ></script>
-<!-- jquery mobile -->
-<script src="assets/mobile/touchSwipe.min.js"></script>
-<!-- jquery mobile -->
-<script src="assets/respond/respond.js"></script>
-<!-- gallery -->
-<script src="assets/gallery/jquery.blueimp-gallery.min.js"></script>
-<!-- custom script -->
-<script src="assets/script.js"></script>
-</body>
+				<a href="#" class="item">
+					<img class="item__image" src="images/ice/1.jpg">
+					<h2 class="item__title">Sunny</h2></a>
+				<a href="#" class="item">
+					<img class="item__image" src="images/ice/5.jpg">
+					<h2 class="item__title">Gloomy</h2></a>
+				<a href="#" class="item">
+					<img class="item__image" src="images/ice/3.jpg">
+					<h2 class="item__title">Snowy</h2></a>
+				<a href="#" class="item">
+					<img class="item__image" src="images/ice/6.jpg">
+					<h2 class="item__title">Icy</h2></a>
+				<a href="#" class="item">
+					<img class="item__image" src="images/ice/4.jpg">
+					<h2 class="item__title">Windy</h2></a>
+				<a href="#" class="item">
+					<img class="item__image" src="images/ice/2.jpg">
+					<h2 class="item__title">Rainy</h2></a>
+			</section>
+		</div><!-- /container -->
+		<script src="js/classie.js"></script>
+		<script>
+			(function() {
+				var container = document.getElementById( 'container' ),
+					trigger = container.querySelector( 'button.trigger' );
+				function toggleContent() {
+					if( classie.has( container, 'container--open' ) ) {
+						classie.remove( container, 'container--open' );
+						classie.remove( trigger, 'trigger--active' );
+						window.addEventListener( 'scroll', noscroll );
+					}
+					else {
+						classie.add( container, 'container--open' );
+						classie.add( trigger, 'trigger--active' );
+						window.removeEventListener( 'scroll', noscroll );
+					}
+				}
+				function noscroll() {
+					window.scrollTo( 0, 0 );
+				}
+				// reset scrolling position
+				document.body.scrollTop = document.documentElement.scrollTop = 0;
+				// disable scrolling
+				window.addEventListener( 'scroll', noscroll );
+				trigger.addEventListener( 'click', toggleContent );
+				[].slice.call( document.querySelectorAll('.items-wrap a') ).forEach( function(el) { el.onclick = function() { return false; } } );
+			})();
+		</script>
+	</body>
 </html>
