f779a3d33deae1df4c29242719ba178ab6431bf1
AJRohrer <andrewjrohrer@gmail.com>
2017-11-19 14:49:18 -0600

    put in test data

--- playlist.php
+++ playlist.php
@@ -32,6 +32,83 @@
                     
                 </section>
             </article>
+
+
+            <article class="rev-item-100">
+                <section class="rev-container-song">
+                    <article class="rev-item-5">
+                        <span class="glyphicon glyphicon-play"></span>
+                    </article>
+
+                    <article class="rev-item-50">
+                        <label>Test Song Title</label>
+                    </article>
+
+                    <article class="rev-item-30">
+                        <label>Test Song Artist</label>
+                    </article>
+
+                    <article class="rev-item-10">
+                        <label>Test Song Length</label>
+                    </article>
+
+                    <article class="rev-item-5">
+                        <span class="glyphicon glyphicon-plus"></span>
+                    </article>
+                    
+                </section>
+            </article>
+
+            <article class="rev-item-100">
+                <section class="rev-container-song">
+                    <article class="rev-item-5">
+                        <span class="glyphicon glyphicon-play"></span>
+                    </article>
+
+                    <article class="rev-item-50">
+                        <label>Test Song Title</label>
+                    </article>
+
+                    <article class="rev-item-30">
+                        <label>Test Song Artist</label>
+                    </article>
+
+                    <article class="rev-item-10">
+                        <label>Test Song Length</label>
+                    </article>
+
+                    <article class="rev-item-5">
+                        <span class="glyphicon glyphicon-plus"></span>
+                    </article>
+                    
+                </section>
+            </article>
+
+            <article class="rev-item-100">
+                <section class="rev-container-song">
+                    <article class="rev-item-5">
+                        <span class="glyphicon glyphicon-play"></span>
+                    </article>
+
+                    <article class="rev-item-50">
+                        <label>Test Song Title</label>
+                    </article>
+
+                    <article class="rev-item-30">
+                        <label>Test Song Artist</label>
+                    </article>
+
+                    <article class="rev-item-10">
+                        <label>Test Song Length</label>
+                    </article>
+
+                    <article class="rev-item-5">
+                        <span class="glyphicon glyphicon-plus"></span>
+                    </article>
+                    
+                </section>
+            </article>
+
         </section>
     </body>
 </html>
