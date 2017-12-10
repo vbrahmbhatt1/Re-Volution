ffe888e42773d6f00cc277d0b5726a3b3866aa8a
vbrahmbhatt1 <vbrahmbhatt1@luc.edu>
2017-12-10 16:03:46 -0600

    added rollback and commit

--- add.php
+++ add.php
@@ -1,9 +1,7 @@
 <?php
-error_reporting(E_ALL ^ E_NOTICE); 
-
+error_reporting(E_ALL ^ E_NOTICE);
 $playlistID=$_POST['addplaylist'];
 $songID=$_POST['addsong'];
-
 try
 {
     echo "<script>console.log( 'Debug Objects: " . $playlistID . " " . $songID . "' );</script>";
@@ -11,18 +9,23 @@ try
     $pdo = new PDO('mysql:host=localhost;dbname=revolution', 'root', '');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->exec('SET NAMES "utf8"');
-
     $link = mysqli_connect("localhost", "root", "", "revolution");
-    
-	if (isset($playlistID) && issset($songID)){
+
+    $pdo->beginTransaction();
+    if (isset($playlistID) && issset($songID)){
         $test = mysqli_query("INSERT INTO PlayListSongs_t VALUES (". $playlistID . "," . $songID . ");");
+      }
     }
-}
+
+    $pdo->commit();
+    echo "New playlist created"
+
 catch (PDOException $e)
 {
+  $pdo->rollBack();
+  echo "Could not create a playlist: ".$e->getMessage();
   $error = 'Unable to connect to the database server.';
   include 'error1.php';
   exit();
 }
-
 ?>
